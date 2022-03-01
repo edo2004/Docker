a6c5343a138897ff643af0c5f6fb3fe489f893ff
Eduar_Ordoñez <eduarordonez@periferia-it.com>
2022-03-01 15:37:09 -0500

    comandos docker 2

--- comandosMontarAmbiente.sh
+++ comandosMontarAmbiente.sh
@@ -9,10 +9,20 @@ chmod 400 Docker/demokey
 scp -i Docker/demokey ubuntu@ec2-34-205-146-122.compute-1.amazonaws.com:/home/ubuntu/bkjenkins.tar .
 echo ______________ Restaurando BACKUP Plugins ______________
 tar -xvf bkjenkins.tar
+rm -rf bkjenkins.tar
 docker restart jenkins
 sleep 20
 echo ______________ Reinicio Terminado ______________
 echo ______________ Iniciando SonarQube p:9100 ______________
-docker-compose -f Docker/SonarQube.yml up -d
+#docker-compose -f Docker/SonarQube.yml up -d
 sleep 20
 echo ______________ SonarQube Iniciado______________
+
+#Exportar Jenkins
+docker commit -a "Eduar" -m "Primera imagen" jenkins jenkins
+docker save -o backup_jenkins.tar jenkins
+
+echo ______________ Imagen de jenkins exportada______________
+
+#Subir al servidor de Aws
+scp -i Docker/demokey backup_jenkins.tar ubuntu@ec2-34-205-146-122.compute-1.amazonaws.com:/home/ubuntu/backupJenkins.tar
