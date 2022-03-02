6b034bf968e3e70eefa777556c93b054b5df944f
edo2004 <53161686+edo2004@users.noreply.github.com>
2022-03-02 15:39:20 -0500

    Update comandosNuevaInstancia.sh
    cambio nombre imagen jenkins en nueva instancia

--- comandosNuevaInstancia.sh
+++ comandosNuevaInstancia.sh
@@ -2,4 +2,4 @@ echo ______________ Descargando BACKUP Plugins ______________
 chmod 400 Docker/demokey
 scp -i Docker/demokey ubuntu@ec2-34-205-146-122.compute-1.amazonaws.com:/home/ubuntu/backupJenkins.tar .
 docker load -i backupJenkins.tar
-docker run -ti --name demoJenkins jenkins
+docker run -ti --name demoJenkins demo-jenkins
